86678055e5de71d82bab11a3335346c38b5710e9
Thomas Hodges <thodges@javascriptonline.org>
2019-02-08 12:38:54 -0800

    formatted_print

---
+++ rust_by_example/chapter01/formatted_print/target/debug/deps/formatted_print-58a1a1bc6bae5ce6.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter01/formatted_print/target/debug/deps/formatted_print-58a1a1bc6bae5ce6: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter01/formatted_print/target/debug/deps/formatted_print-58a1a1bc6bae5ce6.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/chapter01/formatted_print/target/debug/formatted_print.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter01/formatted_print/target/debug/formatted_print: /home/thodges/Workspace/Rust/rust_by_example/chapter01/formatted_print/src/main.rs
