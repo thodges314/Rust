5f7647da30dc00aeb13a94dc66aa9eb26feb8ac4
Thomas Hodges <thodges@javascriptonline.org>
2019-02-08 13:14:04 -0800

    format for struct

---
+++ rust_by_example/chapter01/custom_fmt_display/target/debug/custom_fmt_display.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter01/custom_fmt_display/target/debug/custom_fmt_display: /home/thodges/Workspace/Rust/rust_by_example/chapter01/custom_fmt_display/src/main.rs
---
+++ rust_by_example/chapter01/custom_fmt_display/target/debug/deps/custom_fmt_display-e06b2f32f096b43f.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter01/custom_fmt_display/target/debug/deps/custom_fmt_display-e06b2f32f096b43f: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter01/custom_fmt_display/target/debug/deps/custom_fmt_display-e06b2f32f096b43f.d: src/main.rs
+
+src/main.rs:
