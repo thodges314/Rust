d5c63d35844a9b41a65c16fb45b242c968e5ebe9
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 21:13:58 -0800

    casting

---
+++ rust_by_example/chapter05/casting/target/debug/casting.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/casting/target/debug/casting: /home/thodges/Workspace/Rust/rust_by_example/chapter05/casting/src/main.rs
---
+++ rust_by_example/chapter05/casting/target/debug/deps/casting-e2584fe0fb1a7ad6.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/casting/target/debug/deps/casting-e2584fe0fb1a7ad6: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/casting/target/debug/deps/casting-e2584fe0fb1a7ad6.d: src/main.rs
+
+src/main.rs:
