d021a1c0bf8d28f33b837e37cb30dd3958e54880
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 21:01:29 -0800

    scope and shadowing

---
+++ rust_by_example/chapter04/scope_and_shadowing/target/debug/deps/scope_and_shadowing-1c8bef011937d6cf.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter04/scope_and_shadowing/target/debug/deps/scope_and_shadowing-1c8bef011937d6cf: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter04/scope_and_shadowing/target/debug/deps/scope_and_shadowing-1c8bef011937d6cf.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/chapter04/scope_and_shadowing/target/debug/scope_and_shadowing.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter04/scope_and_shadowing/target/debug/scope_and_shadowing: /home/thodges/Workspace/Rust/rust_by_example/chapter04/scope_and_shadowing/src/main.rs
