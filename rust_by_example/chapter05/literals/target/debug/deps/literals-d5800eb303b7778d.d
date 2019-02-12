5d4bd971888f6eafe0d6f807a121784be20c8d80
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 21:17:31 -0800

    literals

---
+++ rust_by_example/chapter05/literals/target/debug/deps/literals-d5800eb303b7778d.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/literals/target/debug/deps/literals-d5800eb303b7778d: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/literals/target/debug/deps/literals-d5800eb303b7778d.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/chapter05/literals/target/debug/literals.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/literals/target/debug/literals: /home/thodges/Workspace/Rust/rust_by_example/chapter05/literals/src/main.rs
