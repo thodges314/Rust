c82cf4ab11c265240e24b877fb26a13251ee2710
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 23:24:35 -0800

    start chapter 8 - if/else

---
+++ rust_by_example/chapter08/if_else/target/debug/deps/if_else-81c3870098ff7d12.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter08/if_else/target/debug/deps/if_else-81c3870098ff7d12: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter08/if_else/target/debug/deps/if_else-81c3870098ff7d12.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/chapter08/if_else/target/debug/if_else.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter08/if_else/target/debug/if_else: /home/thodges/Workspace/Rust/rust_by_example/chapter08/if_else/src/main.rs
