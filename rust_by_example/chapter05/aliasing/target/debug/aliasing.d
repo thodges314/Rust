709562badc5e2af6e74bd0d94bdb4f60e7990846
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 21:26:43 -0800

    aliasing and end of chapter 5

---
+++ rust_by_example/chapter05/aliasing/target/debug/aliasing.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/aliasing/target/debug/aliasing: /home/thodges/Workspace/Rust/rust_by_example/chapter05/aliasing/src/main.rs
---
+++ rust_by_example/chapter05/aliasing/target/debug/deps/aliasing-e7b5226ab06f458f.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/aliasing/target/debug/deps/aliasing-e7b5226ab06f458f: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/aliasing/target/debug/deps/aliasing-e7b5226ab06f458f.d: src/main.rs
+
+src/main.rs:
