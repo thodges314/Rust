561ae6cc8df8a37e01e004deb9b87004c4bd4be6
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 21:20:10 -0800

    inference

---
+++ rust_by_example/chapter05/inference/target/debug/deps/inference-5d2ab28b50708a8a.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/inference/target/debug/deps/inference-5d2ab28b50708a8a: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/inference/target/debug/deps/inference-5d2ab28b50708a8a.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/chapter05/inference/target/debug/inference.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter05/inference/target/debug/inference: /home/thodges/Workspace/Rust/rust_by_example/chapter05/inference/src/main.rs
