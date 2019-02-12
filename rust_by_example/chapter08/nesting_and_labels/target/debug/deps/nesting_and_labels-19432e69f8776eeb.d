8167bbd8647cde4d1f0efcdb1944365a8b89f52b
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 23:30:26 -0800

    Nesting and labels

---
+++ rust_by_example/chapter08/nesting_and_labels/target/debug/deps/nesting_and_labels-19432e69f8776eeb.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter08/nesting_and_labels/target/debug/deps/nesting_and_labels-19432e69f8776eeb: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter08/nesting_and_labels/target/debug/deps/nesting_and_labels-19432e69f8776eeb.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/chapter08/nesting_and_labels/target/debug/nesting_and_labels.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter08/nesting_and_labels/target/debug/nesting_and_labels: /home/thodges/Workspace/Rust/rust_by_example/chapter08/nesting_and_labels/src/main.rs
