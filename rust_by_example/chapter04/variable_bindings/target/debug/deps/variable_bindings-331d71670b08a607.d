cb74c3b7d8018409e75d5edba67af47095793c6d
Thomas Hodges <thodges@javascriptonline.org>
2019-02-11 20:56:30 -0800

    variable bindings

---
+++ rust_by_example/chapter04/variable_bindings/target/debug/deps/variable_bindings-331d71670b08a607.d
@@ -0,0 +1,5 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter04/variable_bindings/target/debug/deps/variable_bindings-331d71670b08a607: src/main.rs
+
+/home/thodges/Workspace/Rust/rust_by_example/chapter04/variable_bindings/target/debug/deps/variable_bindings-331d71670b08a607.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/chapter04/variable_bindings/target/debug/variable_bindings.d
@@ -0,0 +1,1 @@
+/home/thodges/Workspace/Rust/rust_by_example/chapter04/variable_bindings/target/debug/variable_bindings: /home/thodges/Workspace/Rust/rust_by_example/chapter04/variable_bindings/src/main.rs
