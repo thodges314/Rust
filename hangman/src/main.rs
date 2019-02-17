60cb6e90956a981a99bef6ea29f307a936c1054b
Thomas Hodges <thodges@javascriptonline.org>
2019-02-16 22:11:02 -0800

    display letters

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -4,10 +4,18 @@ use rand::Rng;
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Letter {
+	character: char,
+	revealed: bool,
+}
+
 fn main() {
     let selected_word = select_word();
 
-    println!("The selected word is {}.", selected_word);
+    let mut letters = create_letters(&selected_word);
+
+    display_progress(&letters);
+    
 }
 
 fn select_word() -> String {
@@ -25,4 +33,32 @@ fn select_word() -> String {
 	let random_index = rand::thread_rng().gen_range(0, available_words.len());
 
 	String::from(available_words[random_index])
+}
+
+fn create_letters(word: &String) -> Vec<Letter> {
+	let mut letters : Vec<Letter> = Vec::new();
+
+	for c in word.chars() {
+		letters.push( Letter {
+			character: c,
+			revealed: false,
+		});
+	}
+
+	letters
+}
+
+fn display_progress(letters: &Vec<Letter>) {
+	let mut display_string = String::from("Progress:");
+
+	for letter in letters {
+		display_string.push(' ');
+		if letter.revealed {
+			display_string.push(letter.character);
+		} else {
+			display_string.push('-');
+		}
+	}
+
+	println!("{}", display_string);
 }
