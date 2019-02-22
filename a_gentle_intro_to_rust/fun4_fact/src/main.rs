345697a1841154f6bf8693d10097cd40094de414
Thomas Hodges <thodges@javascriptonline.org>
2019-02-22 13:14:49 -0800

    fun4_fact with u128

--- a_gentle_intro_to_rust/fun4_fact/src/main.rs
+++ a_gentle_intro_to_rust/fun4_fact/src/main.rs
@@ -6,20 +6,20 @@ fn fact_recursive(x: u128) -> u128 {
     }
 }
 
-fn fact_iterative(x: u64) -> u64 {
-    let mut acc: u64 = 1;
+fn fact_iterative(x: u128) -> u128 {
+    let mut acc: u128 = 1;
     for num in 2..=x {
         acc *= num;
     }
     acc
 }
 
-fn fact_iterators(x: u64) -> u64 {
+fn fact_iterators(x: u128) -> u128 {
     (1..=x).product()
 }
 
 fn main() {
-    println!("fact_recursive(20) : {}", fact_recursive(20));
-    println!("fact_iterative(20) : {}", fact_iterative(20));
-    println!("fact_iterators(20) : {}", fact_iterators(20));
+    println!("fact_recursive(20) : {}", fact_recursive(34));
+    println!("fact_iterative(20) : {}", fact_iterative(34));
+    println!("fact_iterators(20) : {}", fact_iterators(34));
 }
