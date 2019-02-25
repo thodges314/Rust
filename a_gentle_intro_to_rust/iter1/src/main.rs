73fa501b9a6b439be2ac7a2a6f4370961970a662
Thomas Hodges <thodges@javascriptonline.org>
2019-02-24 18:08:48 -0800

    iter1 documented

--- a_gentle_intro_to_rust/iter1/src/main.rs
+++ a_gentle_intro_to_rust/iter1/src/main.rs
@@ -5,3 +5,5 @@ fn main() {
     assert_eq!(iter.next(), Some(2));
     assert_eq!(iter.next(), None);
 }
+
+// iter is an object with a 'nect' method that returns an 'Option'.  as long at the Option is not 'None' we can call next.
