0d82e40120d21b3e98c4c361d62ccb3d901acb91
Thomas Hodges <thodges@javascriptonline.org>
2019-02-27 22:33:54 -0800

    both ways

--- projects/chapter4/ref_prim_experiment/src/main.rs
+++ projects/chapter4/ref_prim_experiment/src/main.rs
@@ -2,8 +2,14 @@ fn take_prim(c: &u32) {
     println!("The number is {}.", c);
 }
 
+fn take_prim_no_ref(c: u32) {
+    println!("Without a ref its: {}", c);
+}
+
 fn main() {
     let f = 22;
     take_prim(&f);
     println!("The number again is {}", f);
+    take_prim_no_ref(f);
+    println!("And once again here it's {}.", f);
 }
