b15ac56f0e13f632f945bc59f42fcf4f873dbf55
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-02-28 16:40:16 -0800

    starting modules

---
+++ the_rust_programming_language/chapter07/communicator/target/debug/deps/communicator-ac3413710251c9f6.d
@@ -0,0 +1,8 @@
+/Users/thodges/Workspace/Rust/the_rust_programming_language/chapter07/communicator/target/debug/deps/communicator-ac3413710251c9f6.rmeta: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+/Users/thodges/Workspace/Rust/the_rust_programming_language/chapter07/communicator/target/debug/deps/communicator-ac3413710251c9f6.d: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+src/lib.rs:
+src/client.rs:
+src/network/mod.rs:
+src/network/server.rs:
---
+++ the_rust_programming_language/chapter07/communicator/target/debug/deps/communicator-dfb4001f874a2854.d
@@ -0,0 +1,8 @@
+/Users/thodges/Workspace/Rust/the_rust_programming_language/chapter07/communicator/target/debug/deps/libcommunicator-dfb4001f874a2854.rlib: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+/Users/thodges/Workspace/Rust/the_rust_programming_language/chapter07/communicator/target/debug/deps/communicator-dfb4001f874a2854.d: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+src/lib.rs:
+src/client.rs:
+src/network/mod.rs:
+src/network/server.rs:
