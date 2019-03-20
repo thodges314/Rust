2c293804abf663e7382a91252802694ab8964494
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 12:24:58 -0700

    part 2 in syntax

--- learn-rust-in-7-days/day01/syntax/src/main.rs
+++ learn-rust-in-7-days/day01/syntax/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
 	let b = higher(4,2,8);
+	let o = other(5,9);
 	// format!("{} is highest!", b); <-- sets a string value to that result
     println!("{} is highest.", b);
+    println!("{}", o);
 }
 
 fn higher(a:i32, b:u32, c:i8) -> i32 {
@@ -13,4 +15,12 @@ fn higher(a:i32, b:u32, c:i8) -> i32 {
 		res = c as i32;
 	}
 	res
+}
+
+fn other(a: i32, b:i32)->i32 {
+	let mut c = a + b;
+	c = c % 4;
+	c = c / 2;
+	c += 1;
+	c
 }
