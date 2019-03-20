422d9a24d1bb7450886aef87b0adb71275530de7
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 16:33:06 -0700

    add struct

--- learn-rust-in-7-days/day01/enums/src/main.rs
+++ learn-rust-in-7-days/day01/enums/src/main.rs
@@ -1,11 +1,17 @@
 #[derive(Debug)]
-enum Room {
-	Kitchen,
-	Bedroom,
+pub struct Bed {
+	size:i32,
+	count:u32,
+}
+
+#[derive(Debug)]
+pub enum Room {
+	Kitchen(i32),
+	Bedroom(Bed),
 	Lounge,
 }
 
 fn main() {
-	let t = Room::Kitchen;
+	let t = Room::Kitchen(4);
     println!("Hello from the {:?}.", t);
 }
