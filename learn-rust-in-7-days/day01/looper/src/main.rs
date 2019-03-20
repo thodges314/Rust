7edd3aadb6f60b19ad3eec757a2734f9e1178fa2
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 12:55:39 -0700

    break labeled loop

--- learn-rust-in-7-days/day01/looper/src/main.rs
+++ learn-rust-in-7-days/day01/looper/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     array_loop();
     array_loop_continue();
     array_loop_break();
+    array_loop_break_outer_loop_label();
 }
 
 fn loop_to_10() {
@@ -61,5 +62,18 @@ fn array_loop_break() {
 			break; //breaks out of look
 		}
 		println!("{} - break", n);
+	}
+}
+
+fn array_loop_break_outer_loop_label() {
+	let v = vec![4,7,8,9,11,10];
+	'outer: for i in 0..10 {
+		for n in &v { // borrow v to loop through a *pointer* to the vector - otherwise it's moved here to make the iterator and we can't access it again
+			println!("{} + {} = {} - may break outer", i, n, i+n);
+			if i + n ==15 {
+				break 'outer; //breaks out of look
+			}
+			println!("{} + {} = {} - did not break outer", i, n, i+n);
+		}
 	}
 }
