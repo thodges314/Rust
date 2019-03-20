b21c6de5d2f91747d1a7f4911c644c9be943bd64
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 12:32:36 -0700

    while loop

--- learn-rust-in-7-days/day01/looper/src/main.rs
+++ learn-rust-in-7-days/day01/looper/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     println!("Hello, world!");
     loop_to_10();
     loop_to_10_for();
+    loop_to_10_while();
 }
 
 fn loop_to_10() {
@@ -16,7 +17,15 @@ fn loop_to_10() {
 }
 
 fn loop_to_10_for() {
-	for n in 0..10 {
+	for n in 0..10 { // 10 is not inclusive
 		println!("Hello {}!", n);
+	}
+}
+
+fn loop_to_10_while() {
+	let mut n = 0;
+	while n < 10 {
+		println!("Hello while {}", n);
+		n += 1;
 	}
 }
