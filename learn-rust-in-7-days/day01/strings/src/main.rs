b9d8bde1f4b67173028a25a4c2c693f805e09359
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 13:10:53 -0700

    enumerate

--- learn-rust-in-7-days/day01/strings/src/main.rs
+++ learn-rust-in-7-days/day01/strings/src/main.rs
@@ -1,9 +1,12 @@
 fn main() {
 	let mut s = String::from("Hello, 中国"); // can mutate String, not &str.  &str points to a static spot in the program's code.
-	println!("s.len() = {}", s.len()); // there are 11 chars
+	println!("s.len() = {}", s.len()); // there are 11 chars but 13 bytes
 	s.push_str("!!!");
 	for c in s.chars() {
 		println!("{}", c);
+	}
+	for (i, c) in s.chars().enumerate() {
+		println!("{} - {}", i, c);
 	}
     for c in s.bytes() {
     	println!("{}", c);
