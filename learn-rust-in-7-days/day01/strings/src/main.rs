ace48e70e3605f115c455dd248c499a1d7acd556
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 13:08:04 -0700

    String vs &str and mutation and length

--- learn-rust-in-7-days/day01/strings/src/main.rs
+++ learn-rust-in-7-days/day01/strings/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-	let s = String::from("Hello, 中国");
+	let mut s = String::from("Hello, 中国"); // can mutate String, not &str.  &str points to a static spot in the program's code.
+	println!("s.len() = {}", s.len()); // there are 11 chars
+	s.push_str("!!!");
 	for c in s.chars() {
 		println!("{}", c);
 	}
