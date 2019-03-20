42fd9baba44174ea7b3cedee05e90a5795304bd2
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 12:41:10 -0700

    vector loop

--- learn-rust-in-7-days/day01/looper/src/main.rs
+++ learn-rust-in-7-days/day01/looper/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     loop_to_10();
     loop_to_10_for();
     loop_to_10_while();
+    array_loop();
 }
 
 fn loop_to_10() {
@@ -16,7 +17,7 @@ fn loop_to_10() {
 	}
 }
 
-fn loop_to_10_for() {
+fn loop_to_10_for() { // rust can loop on any *iterator*
 	for n in 0..10 { // 10 is not inclusive
 		println!("Hello {}!", n);
 	}
@@ -27,5 +28,15 @@ fn loop_to_10_while() {
 	while n < 10 {
 		println!("Hello while {}", n);
 		n += 1;
+	}
+}
+
+fn array_loop() {
+	let mut v = Vec::new();
+	v.push(4);
+	v.push(7);
+	v.push(9);
+	for n in v {
+		println!("{}", n);
 	}
 }
