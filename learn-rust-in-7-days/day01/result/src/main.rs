70fa3630ce4854183bb3fe7252e42388aab9b890
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 17:13:55 -0700

    command line args

--- learn-rust-in-7-days/day01/result/src/main.rs
+++ learn-rust-in-7-days/day01/result/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::env::args; // function that returns an iterator over the arguments that the program was supplied
 
 fn main() {
     println!("Hello, world!");
@@ -18,8 +19,29 @@ fn main() {
 
     println!("{}", r);
 
-    match "berb".parse::<f32>() {	// can append with .unwrap(), .unwrap_or(), .expect() (which gives us error data we type in)
-    	Ok(v) => println!("OK - {}", v),	// Ok and Err are of the *result* type
-    	Err(e) => println!("ERROR - {}", e),
-    }
+    // match "berb".parse::<f32>() {	// can append with .unwrap(), .unwrap_or(), .expect() (which gives us error data we type in)
+    // 	Ok(v) => println!("OK - {}", v),	// Ok and Err are of the *result* type
+    // 	Err(e) => println!("ERROR - {}", e),
+    // }
+
+	match get_arg(3) {
+		Ok(v) => println!("OK - {}", v),
+		Err(e) => println!("ERROR - {}", e),
+	}    
 }
+
+fn get_arg(n:usize)->Result<String, String>{
+	for (i,a) in args().enumerate() {	// args are the things passed in on the command like after --
+		if i == n  {
+			return Ok(a);
+		}
+	}
+	Err("Not enough args".to_string())
+}
+
+// thodges@CC-Admin’s-MacBook-Pro-(4):~/Workspace/Rust/learn-rust-in-7-days/day01/result$ cargo run -- h i p d
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.05s
+//      Running `target/debug/result h i p d`
+// Hello, world!
+// NOTHING
+// OK - p
