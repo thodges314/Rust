d282a40bb68d4291e2477a3a3deef7d46d710ce6
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 17:03:57 -0700

    parse error *result*

--- learn-rust-in-7-days/day01/result/src/main.rs
+++ learn-rust-in-7-days/day01/result/src/main.rs
@@ -17,4 +17,9 @@ fn main() {
     let r = hm.get(&4).unwrap_or(&"NOTHING"); // will return wrapped item or NOTHING - we need a pointer because 'get' always returns a pointer
 
     println!("{}", r);
+
+    match "berb".parse::<f32>() {	// can append with .unwrap(), .unwrap_or(), .expect() (which gives us error data we type in)
+    	Ok(v) => println!("OK - {}", v),
+    	Err(e) => println!("ERROR - {}", e),
+    }
 }
