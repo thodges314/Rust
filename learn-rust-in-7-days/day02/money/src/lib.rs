5c24c0c742b93d1ffb8a00a46f4d304330137a96
Thomas Hodges <thomas.hodges@clearcapital.com>
2019-03-20 18:19:12 -0700

    two way conversion

--- learn-rust-in-7-days/day02/money/src/lib.rs
+++ learn-rust-in-7-days/day02/money/src/lib.rs
@@ -1,17 +1,27 @@
 #[derive(PartialEq,Debug)]
-pub struct USD(i32); // this is a tuple
+pub struct USD(f32); // this is a tuple
 #[derive(PartialEq,Debug)]
-pub struct GBP(i32);
+pub struct GBP(f32);
 #[derive(PartialEq,Debug)]
-pub struct CAD(i32);
+pub struct CAD(f32);
 
 pub trait ToUSD {
 	fn to_usd(&self)->USD;
 }
 
+pub trait FromUSD {
+	fn from_usd(u:&USD)->Self;
+}
+
 impl ToUSD for GBP {
 	fn to_usd(&self) -> USD {
-		USD((self.0 * 132) / 100) // floats are bad for money
+		USD((self.0 * 132.0) / 100.0) // floats are bad for money
+	}
+}
+
+impl FromUSD for CAD {
+	fn from_usd(u:&USD)->Self {
+		CAD((u.0 * 133.0) / 100.0)
 	}
 }
 
@@ -20,8 +30,10 @@ mod tests {
 	use super::*;
     #[test]
     fn it_works() {
-        let g = GBP(200);
+        let g = GBP(200.0);
         let u = g.to_usd();
-        assert_eq!(u, USD(264));
+        assert_eq!(u, USD(264.0));
+        let c = CAD::from_usd(&u);
+        assert_eq!(c, CAD(351.12));
     }
 }
