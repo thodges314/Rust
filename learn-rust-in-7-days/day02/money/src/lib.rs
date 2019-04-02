3b4baaca1d6c123aa892ff7a464224be7468263e
Thomas Hodges <thodges@javascriptonline.org>
2019-04-01 22:51:10 -0700

    more notes on money

--- learn-rust-in-7-days/day02/money/src/lib.rs
+++ learn-rust-in-7-days/day02/money/src/lib.rs
@@ -1,26 +1,27 @@
-#[derive(PartialEq, Debug)] // checks for equality where there is not a full equivalance relation.
+#[derive(PartialEq, Debug)] // PartialEq checks for equality where there is not a full equivalance relation.
 pub struct USD(f32); // this is a tuple
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug)] // both PartialEq and Debug needed for assert_eq
 pub struct GBP(f32);
 #[derive(PartialEq, Debug)]
 pub struct CAD(f32);
 
 pub trait ToUSD {
-    fn to_usd(&self) -> USD;
+    fn to_usd(&self) -> USD; // required methods for implementstion have a semicolon instead of swiglly braces
     fn convert<T: FromUSD>(&self) -> T {
-        // worts when T implements FromUSD
+        // T can be *any* type (this is generics)
+        // <T: FromUSD> meant that this worts when T implements FromUSD
         // takes a type parameter of T and self, when called in the code this function will be created for each type
-        T::from_usd(&self.to_usd())
+        T::from_usd(&self.to_usd()) // we are implementing this method so it's not required.
     }
 }
 
 pub trait FromUSD {
-    fn from_usd(u: &USD) -> Self;
+    fn from_usd(u: &USD) -> Self; // is a constructod so it doesn't require self - returns type Self (whatever type is implementing fromUSD)
 }
 
 impl ToUSD for GBP {
     fn to_usd(&self) -> USD {
-        USD((self.0 * 132.0) / 100.0) // floats are bad for money
+        USD((self.0 * 132.0) / 100.0) // floats are bad for money - .0 is first member of tuple of one element
     }
 }
 
@@ -30,11 +31,11 @@ impl FromUSD for CAD {
     }
 }
 
-// Use cargo test to test these.
+// Use `cargo test` to test these.
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::*; // this module uses everything from it's containing module
     #[test]
     fn it_works() {
         let g = GBP(200.0);
